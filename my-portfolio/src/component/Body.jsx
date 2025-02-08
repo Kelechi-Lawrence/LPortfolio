a7222f8981e7c89408d3969892cb2cac9b696ca9
Okoromike Kelechi <mlaw1834@gmail.com>
2025-02-08 17:41:12 +0100

    uploads

---
+++ my-portfolio/src/component/Body.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import profileImage from "../assets/profile.jpg";
+import "./body.css";
+import { FaWhatsapp } from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaSquareGithub } from "react-icons/fa6";
+const Body = () => {
+  return (
+    <div className="body-container">
+      <div className="body-content">
+        <div className="container-title">
+          <h1> Hi i'm Lawrence</h1>
+        </div>
+        <p className="container-description">
+          Creative and passionate Software Engineer experienced in dveloping
+          scalable full stack applications,i enjoy tackling challenges, writing
+          clean code, and bringing impactful ideas to life .
+        </p>
+        <a href="" className="contact-btn">
+          <FaWhatsapp />
+          <CgMail />
+          <IoLogoLinkedin />
+          <FaSquareGithub />
+        </a>
+      </div>
+      <img src={profileImage} className=" profile-img" alt="profile" />
+      <div className="topblur"></div>
+      <div className="bottomblur"></div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ my-portfolio/src/component/Contact.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { CgMail } from "react-icons/cg";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaSquareGithub } from "react-icons/fa6";
+import "./contact.css";
+
+function Contact() {
+  return (
+    <div className="fcontact">
+      <div className="footer">
+        <h2 className="footer-title"> Contact</h2>
+      </div>
+      <div className="contact-links">
+        <ul>
+          <li>
+            <a href="mailto:lawrenceike24@gmail.com">
+              <CgMail />
+              lawrenceike24@gmail.com
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/your-linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoLinkedin />
+              My LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Kelechi-Lawrence"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaSquareGithub />
+              My GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Contact;
